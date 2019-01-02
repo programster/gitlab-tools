cc0007f5274b40485e1c710b879430536dc915c1
Programster <sdpagent@gmail.com>
2019-01-02 14:43:06 +0000

    removing settings template file and loading settings from environment file with logic added to bootstrap file

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -6,9 +6,38 @@
  * and open the template in the editor.
  */
 
-require_once(__DIR__ . '/settings.php');
 require_once(__DIR__ . '/vendor/autoload.php');
 
+if (!file_exists(__DIR__ . '/.env'))
+{
+    die("Missing require environment file.");
+}
+
+$dotEnv = new \Symfony\Component\Dotenv\Dotenv();
+$dotEnv->load(__DIR__ . '/.env');
+
+$requiredVars = array(
+    "ENVIRONMENT",
+    "GITLAB_URL",
+    "GITLAB_ACCESS_TOKEN",
+    "MYSQL_HOST",
+    "MYSQL_DATABASE",
+    "MYSQL_USER",
+    "MYSQL_PASSWORD"
+);
+
+foreach ($requiredVars as $requiredVar)
+{
+    $value = getenv($rquiredVar);
+    
+    if ($value === false)
+    {
+        // environment variable was not set
+        throw new Exception("Missing require environment variable: " . $requiredVar);
+    }
+    
+    define($requiredVar, $value);
+}
 
 new iRAP\Autoloader\Autoloader([
     __DIR__,
