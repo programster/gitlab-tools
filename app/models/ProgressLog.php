3f581edea24a122f1f14593a3bcf8cc1dc72465c
Programster <sdpagent@gmail.com>
2019-01-02 20:43:52 +0000

    adds model for progress log tabl

---
+++ app/models/ProgressLog.php
@@ -0,0 +1,65 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class ProgressLog
+{
+    private $id;
+    private $project_id;
+    private $seconds_remaining;
+    private $seconds_worked;
+    private $open_issues;
+    private $closed_issues;
+    private $when;
+    
+    
+    private function __construct()
+    {
+        
+    }
+    
+    
+    /**
+     * Load all the progress logs for a project. Useful for generating reports/burndown.
+     * @param Project $project
+     * @return array
+     * @throws Exception
+     */
+    public static function loadForProject(Project $project) : array
+    {
+        $logs = array();
+        $db = SiteSpecific::getDb();
+        $query = "SELECT * FROM `progress_log` where `project_id`={$project->getId()} ORDER BY `when` ASC";
+        $result = $db->query($query);
+        
+        if ($result === FALSE)
+        {
+            throw new Exception("Failed to grab progress logs for project");
+        }
+        
+        /* @var $result mysqli_result */
+        while (($object = $result->fetch_object(__CLASS__)) != null)
+        {
+            $logs[] = $object;
+        }
+        
+        if (count($logs) == 0)
+        {
+            die($query);
+        }
+        
+        return $logs;
+    }
+    
+    
+    # Accessors
+    public function getWhen() : int { return $this->when; }
+    public function getSecondsRemaining() : int { return $this->seconds_remaining; }
+    public function getSecondsWorked() : int { return $this->seconds_worked; }
+    public function getNumberOfIssuesOpen() : int { return $this->open_issues; }
+    public function getNumberOfIssuesClosed() : int { return $this->closed_issues; }
+}
